2367f49a43a1188f7cb8b5e190cf3e8cc53c461e
Eesha-Jain <eesha.jain801@gmail.com>
2022-01-22 20:54:27 -0800

    added more resources

--- pages/resources.js
+++ pages/resources.js
@@ -32,20 +32,6 @@ export default function Resources() {
             </div>
 
             <div className="grid-item">
-              <h4>FTC Freight Frenzy - Points Condensed Version</h4>
-              <img src={require(`/public/images/ftcfreightfrenzypointsystem.png`).default.src} width="90%" />
-            </div>
-
-            <div className="grid-item">
-              <h4>FTC Freight Frenzy - Pre-Match Scouting Sheet </h4>
-              <img src={require(`/public/images/prematchscouting.png`).default.src} width="80%" />
-              <br />
-              <br />
-
-              <Link href="/documents/FTCPreMatchScouting.pdf" target="_blank" download id="ftcprematchdownloadbutton"><a className="button">Download Now</a></Link>
-            </div>
-
-            <div className="grid-item">
               <h4>FLL Programming Tutorials</h4>
               <p>This is a Youtube playlist for FLL teams struggling with FLL programming. This series includes videos on how to follow a straight line smoothly, lifting an arm while moving, follow a line with a sharp curve, and more! Check them out!</p>
 
@@ -57,6 +43,34 @@ export default function Resources() {
               <p>FTC Scout is a resource for FTC teams to use to scout throughout the season. This incorporates pre-match and during-match scouting while allowing you to view the capabilities of other teams on the platform. Check it out now!</p>
 
               <a href="https://ftcscout.herokuapp.com/" className="button">Scout Now</a>
+            </div>
+
+            <div className="grid-item">
+              <h4>FTC Shipping Element Detector</h4>
+              <p>Detect any Team Shipping Element for the FTC 2021-2022 Freight Frenzy season. This is a one-of-a-kind solution for autonomous that is easy to implement!</p>
+
+              <a href="https://github.com/HiiDeff/ShippingElementDetector" className="button">View Now</a>
+            </div>
+
+            <div className="grid-item">
+              <h4>Connecting with Professionals Series</h4>
+              <p>Every month, we invite a professional in the STEM field to share their career experiences to students in our local community and answer questions through Everstem Education YouTube live streams. We believe that professionals in the industry can engage and  help students in and out of FIRST learn about what they want to pursue and prepare for a career in.</p>
+
+              <a href="https://www.youtube.com/c/EverstemEducation" className="button">Watch Now</a>
+            </div>
+
+            <div className="grid-item">
+              <h4>FTC Freight Frenzy - Points Condensed Version</h4>
+              <img src={require(`/public/images/ftcfreightfrenzypointsystem.png`).default.src} width="90%" />
+            </div>
+
+            <div className="grid-item">
+              <h4>FTC Freight Frenzy - Pre-Match Scouting Sheet </h4>
+              <img src={require(`/public/images/prematchscouting.png`).default.src} width="80%" />
+              <br />
+              <br />
+
+              <Link href="/documents/FTCPreMatchScouting.pdf" target="_blank" download id="ftcprematchdownloadbutton"><a className="button">Download Now</a></Link>
             </div>
           </div>
 

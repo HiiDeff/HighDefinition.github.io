47353ec0a84418fe2008ad7aead3339157f25d73
Eesha-Jain <eesha.jain801@gmail.com>
2022-01-30 20:57:54 -0800

    update event and images

--- components/banner.js
+++ components/banner.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import Link from 'next/link';
 
 export default function Banner() {
-    useEffect(() => {
+    /*useEffect(() => {
         //Local storage with banner
         if (localStorage.getItem('banner1') == null || localStorage.getItem('timeClosed') == null) {
             localStorage.setItem('banner1', 'block');
@@ -22,11 +22,11 @@ export default function Banner() {
             document.getElementById("banner1").style.display = localStorage.getItem('banner1');
             localStorage.setItem('timeClosed', Date.now());
         });
-    }, []);
+    }, []);*/
 
     return (
         <div className="Banner">
-            <div className="row banner-bar" style={{textAlign: 'center'}} id="banner1">
+            {/*<div className="row banner-bar" style={{textAlign: 'center'}} id="banner1">
                 <div className="col banner-left">
                     <p>Sign up for Washington Tech Invitationals <Link style={{color: 'rgb(180, 0, 180)'}} target="_blank" href="/events">here!</Link></p>
                 </div>
@@ -34,7 +34,7 @@ export default function Banner() {
                 <div className="col banner-right">
                     <p><span className="banner-x" id="banner-x"><FontAwesomeIcon icon={faTimes} /></span></p>
                 </div>
-            </div>
+            </div>*/}
         </div>
     );
 }

667dc51c00331510852505ea73ecbaa3a4b51f38
Eesha-Jain <eesha.jain801@gmail.com>
2022-02-05 13:18:07 -0800

    add id

--- pages/resources.js
+++ pages/resources.js
@@ -22,41 +22,41 @@ export default function Resources() {
               <h4>Introduction to FTC Programming</h4>
               <p>This is a beginner's guide to FTC Programming from tele-op, servors, motors, sensors, and more. You will learn how to operate the control hub along with the station phones, and how to connect these to your Java code. Additionally, you will learn version code and its importance in FTC.</p>
 
-              <a href="https://everstem.org/ftc18225/ftc-programming-guide-beginner.htm" className="button">Read Now</a>
+              <a href="https://everstem.org/ftc18225/ftc-programming-guide-beginner.htm" className="button" id="ftcprogrammingguide">Read Now</a>
             </div>
             <div className="grid-item">
               <h4>Introduction to FTC Robot Hardware</h4>
               <p>This is a beginner's guide to FTC Robot Hardware from building a basic chassis, attaching arm mechanisms, using Onshape as a CAD software, and more. This guide will teach you the skills necessary to create a sample robot with instructions at the end of the guide.</p>
 
-              <a href="https://everstem.org/ftc18225/ftc-robot-guide-beginner.htm" className="button">Read Now</a>
+              <a href="https://everstem.org/ftc18225/ftc-robot-guide-beginner.htm" className="button" id="ftcrobotguide">Read Now</a>
             </div>
 
             <div className="grid-item">
               <h4>FLL Programming Tutorials</h4>
               <p>This is a Youtube playlist for FLL teams struggling with FLL programming. This series includes videos on how to follow a straight line smoothly, lifting an arm while moving, follow a line with a sharp curve, and more! Check them out!</p>
 
-              <a href="https://www.youtube.com/playlist?list=PLUHAVG5dAdrM8x77VdUg-EOhq5HwJ2RLn" className="button">Watch Now</a>
+              <a href="https://www.youtube.com/playlist?list=PLUHAVG5dAdrM8x77VdUg-EOhq5HwJ2RLn" className="button" id="fllprogrammingtutorial">Watch Now</a>
             </div>
 
             <div className="grid-item">
               <h4>FTC Scout</h4>
               <p>FTC Scout is a resource for FTC teams to use to scout throughout the season. This incorporates pre-match and during-match scouting while allowing you to view the capabilities of other teams on the platform. Check it out now!</p>
 
-              <a href="https://ftcscout.herokuapp.com/" className="button">Scout Now</a>
+              <a href="https://ftcscout.herokuapp.com/" className="button" id="ftcscout">Scout Now</a>
             </div>
 
             <div className="grid-item">
               <h4>FTC Shipping Element Detector</h4>
               <p>Detect any Team Shipping Element for the FTC 2021-2022 Freight Frenzy season. This is a one-of-a-kind solution for autonomous that is easy to implement!</p>
 
-              <a href="https://github.com/HiiDeff/ShippingElementDetector" className="button">View Now</a>
+              <a href="https://github.com/HiiDeff/ShippingElementDetector" className="button" id="ftcshippingelementdetector">View Now</a>
             </div>
 
             <div className="grid-item">
               <h4>Connecting with Professionals Series</h4>
               <p>Every month, we invite a professional in the STEM field to share their career experiences to students in our local community and answer questions through Everstem Education YouTube live streams. We believe that professionals in the industry can engage and  help students in and out of FIRST learn about what they want to pursue and prepare for a career in.</p>
 
-              <a href="https://www.youtube.com/c/EverstemEducation" className="button">Watch Now</a>
+              <a href="https://www.youtube.com/c/EverstemEducation" className="button" id="connectingwprofessionals">Watch Now</a>
             </div>
 
             <div className="grid-item">
@@ -70,7 +70,7 @@ export default function Resources() {
               <br />
               <br />
 
-              <Link href="/documents/FTCPreMatchScouting.pdf" target="_blank" download id="ftcprematchdownloadbutton"><a className="button">Download Now</a></Link>
+              <Link href="/documents/FTCPreMatchScouting.pdf" target="_blank" download id="ftcprematchdownloadbutton"><a className="button" id="ftcprematchscouting">Download Now</a></Link>
             </div>
           </div>
 
@@ -83,7 +83,7 @@ export default function Resources() {
               <br />
               <br />
 
-              <a href="https://www.bwtlink.com/ProductItem.aspx?PID=2" className="button">Buy Now</a>
+              <a href="https://www.bwtlink.com/ProductItem.aspx?PID=2" className="button" id="bwtlinkslides">Buy Now</a>
             </div>
 
             <div className="grid-item">
@@ -92,7 +92,7 @@ export default function Resources() {
               <br />
               <br />
 
-              <a href="https://www.bwtlink.com/ProductItem.aspx?PID=4" className="button">Buy Now</a>
+              <a href="https://www.bwtlink.com/ProductItem.aspx?PID=4" className="button" id='bwtlinkscrews'>Buy Now</a>
             </div>
           </div>
         </div>

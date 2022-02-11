55fabb5f755df7b9672828f2475fe7701189a109
Eesha-Jain <eesha.jain801@gmail.com>
2022-02-10 21:49:53 -0800

    update banner w/ world championship

--- components/banner.js
+++ components/banner.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import Link from 'next/link';
 
 export default function Banner() {
-    /*useEffect(() => {
+    useEffect(() => {
         //Local storage with banner
         if (localStorage.getItem('banner1') == null || localStorage.getItem('timeClosed') == null) {
             localStorage.setItem('banner1', 'block');
@@ -22,19 +22,19 @@ export default function Banner() {
             document.getElementById("banner1").style.display = localStorage.getItem('banner1');
             localStorage.setItem('timeClosed', Date.now());
         });
-    }, []);*/
+    }, []);
 
     return (
         <div className="Banner">
-            {/*<div className="row banner-bar" style={{textAlign: 'center'}} id="banner1">
+            <div className="row banner-bar" style={{textAlign: 'center'}} id="banner1">
                 <div className="col banner-left">
-                    <p>Sign up for Washington Tech Invitationals <Link style={{color: 'rgb(180, 0, 180)'}} target="_blank" href="/events">here!</Link></p>
+                    <p>Help fund our team's journey to the FTC World Championship 2022 <a style={{color: 'rgb(0, 0, 180)'}} target="_blank" href="https://gofund.me/7da764b4">now!</a></p>
                 </div>
 
                 <div className="col banner-right">
                     <p><span className="banner-x" id="banner-x"><FontAwesomeIcon icon={faTimes} /></span></p>
                 </div>
-            </div>*/}
+            </div>
         </div>
     );
 }

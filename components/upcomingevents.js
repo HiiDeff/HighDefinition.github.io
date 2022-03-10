39ee63bbb60308b80645a48798015e42e9385c4e
Eesha-Jain <eesha.jain801@gmail.com>
2022-03-09 22:37:26 -0800

    update upcoming events

--- components/upcomingevents.js
+++ components/upcomingevents.js
@@ -8,9 +8,16 @@ export default function UpcomingEvents() {
     <>
       <FadeInSection><div className="section">
         <Title name="Upcoming or Current Events" />
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
-          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faTrophy} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>World Championship</b>: April</p></span>
-        </div>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}><div style={{display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column'}}>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>CADTable Workshop - Building Parts</b>: March 21st</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>CADTable Workshop - Integrating Parts into Assemblies</b>: March 23rd</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>Washington STEM Advocacy Information Night</b>: March 24th</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>CADTable Workshop - Rendering CAD</b>: March 25th</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>Robotics Reactor Meetup</b>: March 26th</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>CADTable</b>: March 27th - April 3rd</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faCalendarAlt} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>FIRST Demonstration</b>: April 2nd</p></span>
+          <span style={{display: 'flex', alignItems: 'center'}}><FontAwesomeIcon icon={faTrophy} width="20" height="20" className="icon"/> &nbsp; &nbsp; <p style={{fontWeight: 300}}><b>World Championship</b>: April 20 - 24</p></span>
+        </div></div>
       </div></FadeInSection>
     </>
   );

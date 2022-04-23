0e6f119b8e092101b6c8f2e66a4056741eb4f5a1
Eesha-Jain <eesha.jain801@gmail.com>
2022-04-22 17:28:08 -0700

    testing new measurement id

--- components/top.js
+++ components/top.js
@@ -10,12 +10,12 @@ export default function Top({name}) {
             <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
             <meta property="og:image" content="/logo.png" />
             
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QCB5JEYRBH"></Script>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VWJ18NMGVG"></Script>
             <Script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-QCB5JEYRBH');
+                gtag('config', 'G-VWJ18NMGVG');
             `}}></Script>
 
             <link rel="icon" href="/logo.png" />    

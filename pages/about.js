16879929a9f40c6efd8f9623306eff34a358f604
Eesha-Jain <eesha.jain801@gmail.com>
2022-05-01 16:20:08 -0700

    moved image gallery

--- pages/about.js
+++ pages/about.js
@@ -5,29 +5,10 @@ import Footer from '../components/footer';
 import Title from '../components/title';
 import Banner from '../components/banner';
 import FadeInSection from '../components/fadeinsection';
-import Gallery from 'react-photo-gallery';
 import React, {useEffect, useState} from 'react';
 React.useLayoutEffect = React.useEffect;
 
 export default function About() {
-  const [photos, setPhotos] = useState([]);
-
-  useEffect(() => {
-    const list = ['19qxmeBZqmIU4DqapqZ-asCH0ZI0PmXjl', '1kiPlmSqABh5b2tgAabVo63gGRBy-880M', '10N0QTdJ1niOvFId6FJhAre1gFXqg2Klp', '1AczoxNcVOqfEgtFM2NIGlSxyPfuP1WXf', '16iBRlSuusuQWWdc7n4zN_4EjrA_qp3j1', '1YuDu00SGYjAseZcK8xndL71J6Vvdljdn', '19YApSunLS6OIn4wC9_qdzggmMAJZ6yMz', '1TYXiRAL0BbwZXlTpLPBLdMoiA4Su9YOZ', '1wNG5X3qdwPMo32vqp7EdiW7yGvuMb87P', '1o3TPM9Jbg0MYlQyf7FUq9VEGXAUSjfLN', '1_gf1M7m4ni9oMsbZSBrKknJm0c3UKBEl', '1kEg5G1yAOezO3Smg8onx7d9Mn-A6eRQu', '1WL8YXLPc2lEKiCXbfmBeniBDz087Gpxe', '1dcduMy020F2y8SCZ24fLl-DVxZqul0Sf', '16AO0Oqap6MoZS_jZWV2KfoniDk8qgCnB', '1cwC2bHPXyzIVvmpOTfGwjciGLWL9aRC-', '1Hf9Y5vssA4KLeP1k4mGZA9JqNJE0m-cr', '1fn4PqBO2hwd4jVn1DEN4EeXXdhaX1LzW', '1VfCbue92A9EDRuAi7Jkh-7cmgomYQX7P', '1KQt53ADSeLs1TooP2kZLTsrVIEL17ILC', '17Em4HE3xsWYtiMjLhPUNAzJk5BErficy', '1wKvty2CRzpUnNKwWKQnNpmAdQNc2y0L_', '1R56XOd2xthCgBYBMBDUvQXOb-pyBsoK5', '1asJoPEA63zG4syGieA2G1dZGxEmPVX8Y', '1hgf2l6-rEuval20el9X0iOR0uqe5th2m', '1YEomn1oFOPakWkBEHC8SM-f1Yh-qdeuj', '17XcLOfWgnui08mz0IOdD7EWmNjho-YQ3', '1ShacQP5pHH49TrD9liC3Jw_wHcwshqdl']
-
-    const sample = [];
-
-    list.forEach((item) => {
-      sample.push({
-        src: 'https://drive.google.com/uc?export=view&id=' + item,
-        height: 300,
-        width: 300,
-      })
-    })
-
-    setPhotos(sample);
-  }, []);
-
   return (
     <>
       <Header title="About" page="About" description="Meet the Team!" />
@@ -215,11 +196,6 @@ export default function About() {
               <p style={{fontWeight: 300}}>I'm Rishabh, and I have 5 years of FIRST experience in both FTC and FRC. As a programmer, I have learned many techniques and solutions to common issues over my journey in FIRST! In my free time, I sketch cars, learn about new technologies, and play basketball for fun.</p>
             </div>
           </div>
-        </div></FadeInSection>
-
-        <FadeInSection><div className="section">
-          <Title name="Photo Gallery" />
-          <Gallery photos={photos} />
         </div></FadeInSection>
       </div>
       

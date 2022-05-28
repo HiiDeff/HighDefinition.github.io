b51396e9eacc82fbb622f7275bc5bfa036239cf5
Eesha-Jain <eesha.jain801@gmail.com>
2022-05-27 21:37:37 -0700

    changed analytics

--- components/top.js
+++ components/top.js
@@ -10,12 +10,12 @@ export default function Top({name}) {
             <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
             <meta property="og:image" content="/logo.png" />
             
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VWJ18NMGVG"></Script>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HMB95Q5EXV"></Script>
             <Script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-VWJ18NMGVG');
+                gtag('config', 'G-HMB95Q5EXV');
             `}}></Script>
 
             <link rel="icon" href="/logo.png" />    

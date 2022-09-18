747c9b01ec6b2df41534238d0c23f9d0a4921386
Eesha-Jain <eesha.jain801@gmail.com>
2022-09-17 19:47:32 -0700

    update description

--- components/top.js
+++ components/top.js
@@ -1,15 +1,21 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-export default function Top({name}) {
-    return (
-        <Head>
-            <title>High Definition | {name}</title>
-            <meta name="description" content="We are First Tech Challenge (FTC) Team 18225 High Definition, a 2nd year veteran robotics team from Bellevue, Washington composed of 13 enthusiastic students grades 7-12. Our mission is to promote STEM and FIRST within our community to inspire the next generation of innovators to solve challenges of the future! We spread mechanical, programming, business, and other practices within the community to ensure their success."/>
+export default function Top({ name }) {
+  return (
+    <Head>
+      <title>High Definition | {name}</title>
+      <meta
+        name="description"
+        content="We are First Tech Challenge (FTC) Team 18225 High Definition, a 3rd year veteran robotics team from Bellevue, Washington composed of 12 enthusiastic students grades 7-12. Our mission is to promote STEM and FIRST within our community to inspire the next generation of innovators to solve challenges of the future! We spread mechanical, programming, business, and other practices within the community to ensure their success."
+      />
 
-            <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-            <meta property="og:image" content="/logo.png" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1"
+      />
+      <meta property="og:image" content="/logo.png" />
 
-            <link rel="icon" href="/logo.png" />    
-        </Head>
-    );
+      <link rel="icon" href="/logo.png" />
+    </Head>
+  );
 }
